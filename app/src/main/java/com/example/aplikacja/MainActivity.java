08f86fd5f572d9badc341e1455eb7f836bf25f0e
Adam524 <36921479+Adam524@users.noreply.github.com>
2019-04-25 13:44:51 +0200

    wypociny :(

--- app/src/main/java/com/example/aplikacja/MainActivity.java
+++ app/src/main/java/com/example/aplikacja/MainActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewParent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     private EditText Email, Haslo;
-    private Button zaloguj, wyloguj;
+    private Button zaloguj, wyloguj,wyjscie;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,36 +36,40 @@ public class MainActivity extends AppCompatActivity {
         Haslo = (EditText) findViewById(R.id.editText2);
         zaloguj = (Button) findViewById(R.id.logowanie);
         wyloguj = (Button) findViewById(R.id.wylogowanie);
+        wyjscie = (Button) findViewById(R.id.exit);
         mAuth = FirebaseAuth.getInstance();
+
+
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                    toastMessage("Udało się zalogować: " + user.getEmail());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                    toastMessage("Udało się wylogować!");
-                }
-                // ...
-            }
+                if(user!=null){
+                    toastMessage("Zalogowano!");
 
+                }
+                else{
+                    toastMessage("NIEEEEEEEEEEEEE!");
+                    }
+            }
 
         };
         zaloguj.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 String email = Email.getText().toString();
                 String pass = Haslo.getText().toString();
                 if(!email.equals("") && !pass.equals("")){
                     mAuth.signInWithEmailAndPassword(email,pass);
-                }else{
+                    Graj(view);
+
+                }else {
                     toastMessage("Nie wypełnione pola!");
                 }
+
+
             }
         });
         wyloguj.setOnClickListener(new View.OnClickListener() {
@@ -72,7 +79,16 @@ public class MainActivity extends AppCompatActivity {
                 toastMessage("Wylogowany...");
             }
         });
-        };
+        wyjscie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                System.exit(0);
+            }
+        });
+        }
+
 
     public void Graj (View view)
     {
